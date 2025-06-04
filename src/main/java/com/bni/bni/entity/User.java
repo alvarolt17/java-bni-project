983100b9a8972527c2bbd34cf27397f178a764f4
Alvaro Louis <alvarolouis00@gmail.com>
2025-06-04 10:22:24 +0700

    integrate with openShift

---
+++ src/main/java/com/bni/bni/entity/User.java
@@ -0,0 +1,72 @@
+package com.bni.bni.entity;
+
+import java.math.BigInteger;
+import java.time.OffsetDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    // Getter & Setter untuk username
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter & Setter untuk passwordHash
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    // Getter & Setter untuk role
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/bni/bni/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.bni.bni.service;
+
+import com.bni.bni.entity.User;
+import com.bni.bni.repository.UserRepository;
+import com.bni.bni.util.JwtUtil;
+import org.slf4j.Logger;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private final Logger logger = org.slf4j.LoggerFactory.getLogger(AuthService.class);
+    private final PasswordEncoder encoder;
+    private final JwtUtil jwtUtil;
+    private final UserRepository repo;
+
+    AuthService(PasswordEncoder encoder, JwtUtil jwtUtil, UserRepository repo) {
+        this.encoder = encoder;
+        this.jwtUtil = jwtUtil;
+        this.repo = repo;
+    }
+
+
+    public String register(String username, String password) {
+        if (repo.existsByUsername(username)) {
+            logger.warn("PERCOBAAN REGSTRASI DENGAN MENGGUNAKAN USER TERDAFTAR: {}", username);
+            return "User already exists";
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPasswordHash(encoder.encode(password));
+        user.setRole("USER");
+        repo.save(user);
+
+        logger.warn("PERCOBAAN REGISTER BERHASIL: {}", username);
+        return "Registered successfully";
+    }
+
+    public String login(String username, String password) {
+        Optional<User> user = repo.findByUsername(username);
+        if (user.isPresent() && encoder.matches(password, user.get().getPasswordHash())) {
+            logger.warn("PERCOBAAN LOGIN BERHASIL: {}", username);
+            return jwtUtil.generateToken(username, user.get().getRole());
+        }
+
+        logger.warn("PERCOBAAN LOGIN GAGAL, USERNAME ATAU PASSWORD SALAH: {}", username);
+        return null;
+    }
+}
