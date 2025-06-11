1b4da8d657be23da77da3c8332886c1b11a59be4
Alvaro Louis <alvarolouis00@gmail.com>
2025-06-11 13:36:10 +0700

    300

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -44,7 +44,7 @@ public class AuthController {
         if (token != null) {
             response.put("status", 200);
             response.put("token", token);
-            response.put("message", "Login Berhasil!299");
+            response.put("message", "Login Berhasil!300");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
